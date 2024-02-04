d798edf7c8276c116923ed90ece345e3a7a92036
Dipo George <dipogeorge@imarveldesign.co.uk>
2024-02-04 10:03:50 +0000

    fixed errors on todo repo impl

--- lib/features/todo/data/repositories/todo_repository_impl.dart
+++ lib/features/todo/data/repositories/todo_repository_impl.dart
@@ -4,54 +4,56 @@ import 'package:firstapp/features/todo/domain/entities/todo.dart';
 import 'package:firstapp/features/todo/domain/repositories/todo_repository.dart';
 import 'package:firstapp/shared/errors/failure.dart';
 
+import '../../../../shared/utils/typedef.dart';
+
 class TodoRepositoryImpl implements TodoRepository {
   final TodoRemoteDatabase remoteDatabase;
 
   TodoRepositoryImpl({required this.remoteDatabase});
 
   @override
-  Future<Either<Failure, Todo>> add(Todo todo) async {
+  ResultFuture<Todo> add(Todo todo) async {
     try {
       final results = await remoteDatabase.addTodo(todo);
       return Right(results);
     } catch (e) {
-      return const Left(Failure(
+      return const Left(ApiFailure(
           message: "WHOOPS! Something has gone wrong. Unable to add.",
           statusCode: 500));
     }
   }
 
   @override
-  Future<Either<Failure, Todo>> delete(Todo todo) async {
+  ResultFuture<Todo> delete(Todo todo) async {
     try {
       final results = await remoteDatabase.deleteTodo(todo);
       return Right(results);
     } catch (e) {
-      return const Left(Failure(
+      return const Left(ApiFailure(
           message: "WHOOPS! Something has gone wrong. Unable to delete.",
           statusCode: 500));
     }
   }
 
   @override
-  Future<Either<Failure, Todo>> edit(Todo todo) async {
+  ResultFuture<Todo> edit(Todo todo) async {
     try {
       final results = await remoteDatabase.editTodo(todo);
       return Right(results);
     } catch (e) {
-      return const Left(Failure(
+      return const Left(ApiFailure(
           message: "WHOOPS! Something has gone wrong. Unable to edit.",
           statusCode: 500));
     }
   }
 
   @override
-  Future<Either<Failure, Stream<List<Todo>>>> getAll() async {
+  ResultFuture<Stream<List<Todo>>> getAll() async {
     try {
       final results = remoteDatabase.listTodos();
       return Right(results);
     } catch (e) {
-      return const Left(Failure(
+      return const Left(ApiFailure(
           message: "WHOOPS! Something has gone wrong. Unable to get todos.",
           statusCode: 500));
     }
